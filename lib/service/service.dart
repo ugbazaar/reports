ea68ec54c85ae154dc9030756a6731faaa2b18a4
Kiran Timsina <timsinakiran@gmail.com>
2020-04-13 14:41:08 +0545

    report api added

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -4,15 +4,19 @@ import 'package:http/http.dart' as http;
 import 'package:http_test/model/app_data_model.dart';
 
 class Service {
-  static const url =
-      "https://jsonblob.com/api/jsonBlob/453a628d-7c85-11ea-8070-fbc26612a2b7";
+  static const url = "http://backend.ugbazaar.com/api/y2y?apiKey=112233445500";
+  // static const url =
+  //     "https://jsonblob.com/api/jsonBlob/453a628d-7c85-11ea-8070-fbc26612a2b7";
+
   Future<AppDataModel> getTestData() async {
+    print('here');
     var response = await http.get(url);
+    print('there');
     print("ResponseBody ===>>  ${response.body.toString()}");
-    if(response.statusCode==200){
+    if (response.statusCode == 200) {
       Map<String, dynamic> responseMap = jsonDecode(response.body);
-       AppDataModel result = AppDataModel.fromJson(responseMap);
-       return result;
+      AppDataModel result = AppDataModel.fromJson(responseMap);
+      return result;
     }
   }
 }
